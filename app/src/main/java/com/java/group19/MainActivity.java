b830eb7205b760092102d123bd6cfaf75c2db50b
Xu Yi <xy_xuyi@foxmail.com>
2017-09-09 14:48:08 +0800

    fix News class; add Keyword class

--- app/src/main/java/com/java/group19/MainActivity.java
+++ app/src/main/java/com/java/group19/MainActivity.java
@@ -3,11 +3,14 @@ package com.java.group19;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.litepal.tablemanager.Connector;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Connector.getDatabase();
     }
 }
