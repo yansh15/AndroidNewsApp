b53fe5186ed586dc766fa434a0aa1b060ddf5f88
Xu Yi <xy_xuyi@foxmail.com>
2017-09-09 21:51:23 +0800

    Complete Config class; Finish DatabaseHelper class

--- app/src/main/java/com/java/group19/MainActivity.java
+++ app/src/main/java/com/java/group19/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Connector.getDatabase();
+        DatabaseHelper.init();
 
         //set toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
